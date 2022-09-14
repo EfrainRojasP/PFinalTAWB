b9fb9bd4601609365913c9229f90ab20983af3e6
EfrainRojas <efrainulisesrojaspina@gmail.com>
2022-09-13 22:20:50 -0500

    Se actualizo la lectura

--- PHP/Clases/Lectura.php
+++ PHP/Clases/Lectura.php
@@ -4,20 +4,22 @@ class Lectura{
     private float $condicionLuz;
     private float $humedad;
     private float $temp;
-    private string $duracionActividad;
+    private string $fechaLectura;
     private Espacio $espacio;
+    private Actividad $actividad;
 
-    public function __construct($condLuz, $hum, $temp, $duraAct, $esp) {
+    public function __construct($condLuz, $hum, $temp, $duraAct, $esp, $actividad) {
         $this->condicionLuz = $condLuz;
         $this->humedad = $hum;
         $this->temp =$temp;
-        $this->duracionActividad = $duraAct;
+        $this->fechaLectura = $duraAct;
         $this->espacio = new Espacio($esp->getIdEspacio(), $esp->getNumEspacio(), $esp->getEdificio(), $esp->getNodos());
+        $this->actividad = new Actividad($actividad->getIdActividad(), $actividad->getNombreActividad());
     }
     
     public function lecturaJSON(): array
     {
-        return array("idLectura" => $this->idLectura, "condLuz" => $this->condicionLuz, "humerdad" => $this->humedad, "temp" => $this->temp, "duraAct" => $this->duracionActividad, "espacio" => $this->espacio);
+        return array("idLectura" => $this->idLectura, "condLuz" => $this->condicionLuz, "humerdad" => $this->humedad, "temp" => $this->temp, "duraAct" => $this->fechaLectura, "espacio" => $this->espacio, "actividad" => $this->actividad);
     }
 
     /**
@@ -81,21 +83,21 @@ class Lectura{
     }
 
     /**
-     * Get the value of duracionActividad
+     * Get the value of fechaLectura
      */ 
-    public function getDuracionActividad()
+    public function getfechaLectura()
     {
-        return $this->duracionActividad;
+        return $this->fechaLectura;
     }
 
     /**
-     * Set the value of duracionActividad
+     * Set the value of fechaLectura
      *
      * @return  self
      */ 
-    public function setDuracionActividad($duracionActividad)
+    public function setfechaLectura($fechaLectura)
     {
-        $this->duracionActividad = $duracionActividad;
+        $this->fechaLectura = $fechaLectura;
 
         return $this;
     }
@@ -116,6 +118,27 @@ class Lectura{
     public function setEspacio($espacio)
     {
         $this->espacio = $espacio;
+
+        return $this;
+    }
+
+
+    /**
+     * Get the value of actividad
+     */ 
+    public function getActividad()
+    {
+        return $this->actividad;
+    }
+
+    /**
+     * Set the value of actividad
+     *
+     * @return  self
+     */ 
+    public function setActividad($actividad)
+    {
+        $this->actividad = $actividad;
 
         return $this;
     }
@@ -145,11 +168,14 @@ class Lectura{
         return "idLectura: ".$this->idLectura.
                 " condicionLuz: " .$this->condicionLuz.
                 " humedad: ".$this->humedad.
-                " Duaracion actividad: ".$this->duracionActividad.
-                " Espacio: ".$this->espacio->toString();
+                " Duaracion actividad: ".$this->fechaLectura.
+                " Espacio: ".$this->espacio->toString().
+                " Actividad: ".$this->actividad->toString();
     }
 
    
+
+    
 }
 
 ?>
